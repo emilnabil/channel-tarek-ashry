d34554b68585da5c6f4685f450e9fa855c298ea3
emil-nabil333 <elbrinselomda@yahoo.com>
2022-08-08 13:48:39 -0700

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,69 +1,170 @@
-#!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################
-VERSION="2022-08-07"  
-MY_URL="https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main"  
-echo "******************************************************************************************************************"
-echo "    download and install channel  "
-echo "============================================================================================================================="
-echo " remove old channel "
-# Remove any Channel  # 
-rm -rf /etc/enigma2/lamedb 
-rm -rf /etc/enigma2/*list 
-rm -rf /etc/enigma2/*.tv 
-rm -rf /etc/enigma2/*.radio 
-rm -rf /etc/tuxbox/*.xml 
-#####################################################################################
-echo "         install channel    "
-cd /tmp
-set -e 
-wget -q  "https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main/channels_backup_by_tarek-ashry_2022-08-07.tar.gz"
-wait
-tar -xzf channels_backup_by_tarek-ashry_2022-08-07.tar.gz  -C /
-wait
+#!/bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL Channel
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main/installer.sh -qO - | /bin/sh
+#
+# ###########################################
+
+###########################################
+# Configure where we can find things here #
+VERSION='2022-08-07'
+TMPDIR='/tmp'
+PACKAGE='astra-sm'
+MY_PACKAGE='channels_backup_by_tarek-ashry'
+MY_URL='https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main'
+
+########################
+VERSION=$(wget $MY_URL/version -qO- | cut -d "=" -f2-)
+
+########################
+BINPATH=/usr/bin
+ETCPATH=/etc
+ASTRAPATH=${ETCPATH}/astra
+######
+BBCPMT=${BINPATH}/bbc_pmt_starter.sh
+BBCPY=${BINPATH}/bbc_pmt_v6.py
+BBCENIGMA=${BINPATH}/enigma2_pre_start.sh
+######
+SYSCONF=${ETCPATH}/sysctl.conf
+ASTRACONF=${ASTRAPATH}/astra.conf
+ABERTISBIN=${ASTRAPATH}/scripts/abertis
+
+########################
+CONFIGpmttmp=${TMPDIR}/bbc_pmt_v6/bbc_pmt_starter.sh
+CONFIGpytmp=${TMPDIR}/bbc_pmt_v6/bbc_pmt_v6.py
+CONFIGentmp=${TMPDIR}/bbc_pmt_v6/enigma2_pre_start.sh
+CONFIGsysctltmp=${TMPDIR}/${PACKAGE}/sysctl.conf
+CONFIGastratmp=${TMPDIR}/${PACKAGE}/astra.conf
+CONFIGabertistmp=${TMPDIR}/${PACKAGE}/abertis
+
+########################
+if [ -f /etc/opkg/opkg.conf ]; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+fi
+
+########################
+rm -rf /etc/enigma2/lamedb
+rm -rf /etc/enigma2/*list
+rm -rf /etc/enigma2/*.tv
+rm -rf /etc/enigma2/*.radio
+rm -rf /etc/tuxbox/*.xml
+
+########################
+install() {
+    if grep -qs "Package: $1" $STATUS; then
+        echo
+    else
+        $OPKG >/dev/null 2>&1
+        echo "   >>>>   Need to install $1   <<<<"
+        echo
+        $OPKGINSTAL "$1"
+        sleep 1
+        clear
+    fi
+}
+
+########################
+if [ $OSTYPE = "Opensource" ]; then
+    for i in dvbsnoop $PACKAGE; do
+        install $i
+    done
+fi
+
+#########################
+case $(uname -m) in
+armv7l*) plarform="arm" ;;
+mips*) plarform="mips" ;;
+esac
+
+#########################
+rm -rf ${ASTRACONF} ${SYSCONF}
+rm -rf ${TMPDIR}/channels_backup_user_"${VERSION}"* astra-* bbc_pmt_v6*
+
+#########################
+echo
+set -e
+echo "Downloading And Insallling Channel Please Wait ......"
+wget $MY_URL/${MY_PACKAGE}_${VERSION}.tar.gz -qP $TMPDIR
+tar -zxf $TMPDIR/${MY_PACKAGE}_${VERSION}.tar.gz -C /
+sleep 5
+set +e
+echo
 echo "   >>>>   Reloading Services - Please Wait   <<<<"
 wget -qO - http://127.0.0.1/web/servicelistreload?mode=0 >/dev/null 2>&1
 sleep 2
-cd ..
-set +e
-rm -f /tmp/channels_backup_by_tarek-ashry_2022-08-07.tar.gz
-sleep 2;
-echo "" 
-echo "Installing astra sm patch - dvbsnoop "
-opkg install astra-sm 
-opkg install dvbsnoop
-sleep 1
-cd /tmp
-set -e
-wget
-"https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main/astra-arm.tar.gz"
-wait
-tar -xzf astra-arm.tar.gz  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/astra-arm.tar.gz
-rm /etc/astra/astra.conf
-wait
-wget https://raw.githubusercontent.com/ciefp/astra.conf/main/astra.conf -P /etc/astra/ && chmod 755 /etc/astra/astra.conf
-chmod 755 /etc/astra/scripts/abertis
-sleep 1
-cd /tmp
-set -e
-wget
-"wget https://raw.githubusercontent.com/emilnabil/channel-tarek-ashry/main/bbc_pmt_v6.tar.gz"
-wait
-tar -xzf bbc_pmt_v6.tar.gz  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/bbc_pmt_v6.tar.gz
-sleep 1
+echo
 
- wait   
+#########################
+if [ -f $BBCPMT ] && [ -f $BBCPY ] && [ -f $BBCENIGMA ]; then
+    echo "   >>>>   All Config BBC Files found   <<<<"
+    sleep 2
+else
+    set -e
+    echo "Downloading And Insallling Config BBC Please Wait ......"
+    wget $MY_URL/bbc_pmt_v6.tar.gz -qP $TMPDIR
+    tar -xzf $TMPDIR/bbc_pmt_v6.tar.gz -C $TMPDIR
+    set +e
+    chmod -R 755 ${TMPDIR}/bbc_pmt_v6
+    sleep 1
+    echo "---------------------------------------------"
+    if [ ! -f $BBCPMT ]; then
+        cp -f $CONFIGpmttmp $BINPATH >/dev/null 2>&1
+        echo "[send (bbc_pmt_starter.sh) file]"
+    fi
+    if [ ! -f $BBCPY ]; then
+        cp -f $CONFIGpytmp $BINPATH >/dev/null 2>&1
+        echo "[send (bbc_pmt_v6.py) file]"
+    fi
+    if [ ! -f $BBCENIGMA ]; then
+        cp -f $CONFIGentmp $BINPATH >/dev/null 2>&1
+        echo "[send (enigma2_pre_start.sh) file]"
+    fi
+    echo "---------------------------------------------"
+fi
+
+#########################
+if [ $OSTYPE = "Opensource" ]; then
+    if [ -f $ASTRACONF ] && [ -f $ABERTISBIN ] && [ -f $SYSCONF ]; then
+        echo "   >>>>   All Config $PACKAGE Files found   <<<<"
+        sleep 2
+    else
+        set -e
+        echo "Downloading Config $PACKAGE Please Wait ......"
+        wget $MY_URL/astra-"${plarform}".tar.gz -qP $TMPDIR
+        tar -xzf $TMPDIR/astra-"${plarform}".tar.gz -C $TMPDIR
+        set +e
+        chmod -R 755 ${TMPDIR}/${PACKAGE}
+        sleep 1
+        echo "---------------------------------------------"
+        if [ ! -f $SYSCONF ]; then
+            cp -f $CONFIGsysctltmp $ETCPATH >/dev/null 2>&1
+            echo "[send (sysctl.conf) file]"
+        fi
+        if [ ! -f $ASTRACONF ]; then
+            cp -f $CONFIGastratmp $ASTRAPATH >/dev/null 2>&1
+            echo "[send (astra.conf) file]"
+        fi
+        if [ ! -f $ABERTISBIN ]; then
+            cp -f $CONFIGabertistmp $ASTRAPATH/scripts >/dev/null 2>&1
+            echo "[send (abertis) file]"
+        fi
+        echo "---------------------------------------------"
+    fi
+fi
+
+#########################
+rm -rf ${TMPDIR}/channels_backup_by_tarek-ashry_2022-08-07.tar.gz
+rm -rf ${TMPDIR}/* astra-
+rm -rf ${TMPDIR}/* bbc_pmt_v6
+
+sync
 echo ""
 echo ""
-echo "" 
-echo "****************************************************************************************************************************"
 echo "*********************************************************"
 echo "#       Channel And Config INSTALLED SUCCESSFULLY       #"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
@@ -82,24 +183,6 @@ else
 fi
 
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
